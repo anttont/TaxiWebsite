fa37e22f12e283dc76ed4dfee30b823783ba4e7d
anttont <anttoni.tornikoski@gmail.com>
2024-09-25 12:02:47 +0300

    New init

---
+++ taksi-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  
+  return (
+    <div className="gradient__bg">
+    <Navbar />
+    
+  </div>
+  )
+}
+
+export default App
