51556a0f8c81985e1a0a3cdf613e560bfa21e2d2
anttont <anttoni.tornikoski@gmail.com>
2024-10-05 21:02:45 +0300

    Button animations

--- taksi-project/src/components/Navbar/Navbar.jsx
+++ taksi-project/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
+import React, { useState } from 'react';
 import "./Navbar.css"
 const Navbar = () => {
+    const [isAnimating, setIsAnimating] = useState(false); // State to trigger animation
+
+    const handleClick = () => {
+        setIsAnimating(true);
+        // Reset animation state after the animation duration
+        setTimeout(() => {
+          setIsAnimating(false);
+        }, 1000); // Duration of animation in milliseconds
+      };
 
     return (
         <div className="">
             <nav className="taksi__navbar">
-                <p>TAKSI KARI TORNIKOSKI </p>
+                <p>KARI TORNIKOSKI TAKSI</p>
 
                 <ul className="taksi__navbar-list">
                 <li>hinnasto</li>
@@ -12,7 +22,7 @@ const Navbar = () => {
                 <li>yhteystiedot</li>
                 </ul>
 
-                <button className="taksi__navbar-button">tilaa</button> 
+                <button  onClick={handleClick} className={`taksi__navbar-button ${isAnimating ? 'leave-screen' : ''}`}>tilaa</button> 
                 </nav>
         </div>
      )
