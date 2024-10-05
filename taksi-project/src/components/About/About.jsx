4744abbca0a8a605c4ff333abf0f12dc028ad77b
anttont <anttoni.tornikoski@gmail.com>
2024-10-05 19:16:59 +0300

    Kari added
    Picture of Kari. Placeholder AI image.

--- taksi-project/src/components/About/About.jsx
+++ taksi-project/src/components/About/About.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './About.css'; 
 import skoda_placeholder from '/src/assets/images/skoda_transparent.png';
+import kari from '/src/assets/images/fakekaritransparent.png';
 
 const About = () => {
   return (
     <div className='about-container'>
+
+<div className='about-image-container'>
       <div className="about-image">
         <img src={skoda_placeholder} alt="skoda" />
       </div>
-      <div className="about-text">
+      <div className="about-image">
+        <img src={kari} alt="kari" />
+      </div>
+      
+    </div>
+
+    <div className="about-text">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       </div>
       
     </div>
+    
   );
 };
 
--- taksi-project/src/components/Pricing/Pricing.jsx
+++ taksi-project/src/components/Pricing/Pricing.jsx
@@ -30,23 +30,23 @@ const Pricing = () => {
     </div>
     <div className="example-container">
     <h2>Esimerkki hinnasto</h2>
-    <div className="pricing-item">
-      <h3>Perusmaksut</h3>
-      <p>Arkisin kello 07:00-18.00 </p>
-      <p>Per mile: $1.50</p>
-      <p>Minimum fare: $5.00</p>
+    <div className="example-item">
+        <p>Base fare: $5.00</p>
     </div>
-    <div className="pricing-item">
-      <h3>Matkataksa</h3>
-      <p>Base fare: $4.00</p>
-      <p>Per mile: $2.00</p>
-      <p>Minimum fare: $7.00</p>
+    <div className="example-item">
+        <p>Base fare: $5.00</p>
     </div>
-    <div className="pricing-item">
-      <h3>Muut</h3>
-      <p>Base fare: $5.00</p>
-      <p>Per mile: $2.50</p>
-      <p>Minimum fare: $10.00</p>
+    <div className="example-item">
+    <   p>Base fare: $5.00</p>
+    </div>
+    <div className="example-item">
+        <p>Base fare: $5.00</p>
+    </div>
+    <div className="example-item">
+        <p>Base fare: $5.00</p>
+    </div>
+    <div className="example-item">
+    <   p>Base fare: $5.00</p>
     </div>
 
     
