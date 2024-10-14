bbc9d1cf1e8a36193b82de0ae19b19f10aa08f4c
anttont <anttoni.tornikoski@gmail.com>
2024-10-14 17:51:21 +0300

    About page rework

--- taksi-project/src/components/About/About.jsx
+++ taksi-project/src/components/About/About.jsx
@@ -4,39 +4,25 @@ import skoda_placeholder from '/src/assets/images/skoda_transparent.png';
 import kari from '/src/assets/images/fakekaritransparent.png';
 import hämeenkatu from '/src/assets/images/hämeenkatucopy.jpg';
 
-const About = () => {
+const HeroSection = () => {
   return (
-    <div className='about-container'>
-
-
-      <div className='about-background-image'>
-          <img src={hämeenkatu} alt="hämeenkatu" />
+    <div className="hero-container">
+      <div className="hero-background">
+        <img src={hämeenkatu} alt="Taxi cars on Hämeenkatu" />
       </div>
-        <div className='about-headtext'>
-          <h1>tervetuloa parhaan taksin kyytiin!</h1>
-          
+      <div className="hero-content">
+        <h2>TAKSI KARI TORNIKOSKI 040 911 911</h2>
+        <h1>Perinteistä taksipalvelua</h1>
+        <p>
+          Taksi Kari Tornikoski takaa luotettavaa perinteistä palvelua edullisin hinnoin.
+        </p>
+        <div className="hero-buttons">
+          <a href="#book" className="btn-primary">Tilaa nyt</a>
+
         </div>
-      <div className='about-image-container'>
-      <div className="about-image animation_right">
-        <img src={skoda_placeholder} alt="skoda" />
       </div>
-      
-      
     </div>
-
-    {/*<div className="about-image animation_left">
-        <img src={kari} alt="kari" /> 
-      </div>*/}
-
-    <div className="about-text">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      </div>
-      
-    </div>
-    
   );
-};
+}
 
-export default About;
+export default HeroSection;
--- taksi-project/src/components/Contact/Contact.jsx
+++ taksi-project/src/components/Contact/Contact.jsx
@@ -7,14 +7,14 @@ const Contact = () => {
     <div className="contact-container">
 
       <div className="contact-box">
-        <h3>YHTEYSTIEDOT</h3>
+        <h3>Yhteystiedot</h3>
         <p>Puhnum: 0501232423</p>
         <p>Sähköposti: taksi@firma.fi</p>
       </div>
 
       <div className="copyright">
-        <p>© 2024 TAKSI KARI TORNIKOSKI</p>
-        <p>sivusto: anttonitornikoski.com</p>
+        <p>© 2024 Taksi Kari Tornikoski</p>
+        <p>Sivusto: anttonitornikoski.com</p>
       </div>
 
       
