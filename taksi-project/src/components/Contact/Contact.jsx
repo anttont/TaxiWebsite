abc7b3278da373582e8b790bec45a99b3810e144
anttont <anttoni.tornikoski@gmail.com>
2024-10-18 10:18:17 +0300

    Updated media queries, small css fixes
    Added href

--- taksi-project/src/components/Contact/Contact.jsx
+++ taksi-project/src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
 
       <div className="copyright">
         <p>© 2024 Kari Tornikoski Taksi</p>
-        <p>Sivusto: anttonitornikoski.com</p>
+        <a href="http://anttonitornikoski.com" >Sivusto: anttonitornikoski.com</a>
       </div>
 
       
